e0d7197d152f531eac0f0e2d77699ccab84ee0f4
Harshal Patil <harshal.patil@in.ibm.com>
2017-04-04 10:38:11 +0530

    added more error handling
    Signed-off-by: Harshal Patil <harshal.patil@in.ibm.com>

--- netinfo.sh
+++ netinfo.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
 IP_ADDR=$(nsenter --net=$1 /sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+if [ -z "$IP_ADDR" ]
+then
+   exit 1
+fi
+
 MAC_ADDR=$(nsenter --net=$1 /sbin/ifconfig eth0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')
+if [ -z "$MAC_ADDR" ]
+then
+   exit 1
+fi
+
 NETMASK=$(nsenter --net=$1 /sbin/ifconfig eth0 | sed -rn '2s/ .*:(.*)$/\1/p')
+if [ -z "$NETMASK" ]
+then
+   exit 1
+fi
+
 GATEWAY=$(nsenter --net=$1 /sbin/ip route | awk '/default/ { print $3 }')
+if [ -z "$GATEWAY" ]
+then
+   exit 1
+fi
 
 echo $IP_ADDR,$MAC_ADDR,$NETMASK,$GATEWAY
-nsenter --net=$1 /sbin/ifconfig eth0 down
+
+nsenter --net=$1 /sbin/ifconfig eth0 down 2>&1 > /dev/null
